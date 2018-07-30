36855fee22ff73e115a8466a977dfc783e9c8fd9
voooza <petr@vanis.org>
2018-07-30 22:14:30 +0200

    project import

---
+++ test/service_poker/core_test.clj
@@ -0,0 +1,7 @@
+(ns service-poker.core-test
+  (:require [clojure.test :refer :all]
+            [service-poker.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
